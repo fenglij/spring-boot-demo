363bf4fab780fb0385cf4ee30f1b9c6981dc03f2
fenglij <feng_lj_89@163.com>
2018-08-21 17:54:00 +0800

    添加常用工具类

--- src/main/java/com/xpower/iot/demo/common/shiro/ShiroConfig.java
+++ src/main/java/com/xpower/iot/demo/common/shiro/ShiroConfig.java
@@ -33,21 +33,29 @@ public class ShiroConfig {
     public ShiroFilterFactoryBean shiroFilterFactoryBean(DefaultWebSecurityManager securityManager) {
         ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
         shiroFilterFactoryBean.setSecurityManager(securityManager);
+        //用户未认证登录的页面
+        shiroFilterFactoryBean.setLoginUrl("/user/login");
+        //用户认证成功登录的页面
+        shiroFilterFactoryBean.setSuccessUrl("/index");
+        //用户没有该权限的展示页面
+        shiroFilterFactoryBean.setUnauthorizedUrl("/403");
 
         Map<String, String> filterChainDefinitionMap = new LinkedHashMap<String, String>();
         //注意过滤器配置顺序 不能颠倒
         //配置退出 过滤器,其中的具体的退出代码Shiro已经替我们实现了，登出后跳转配置的loginUrl
-        filterChainDefinitionMap.put("/logout", "logout");
-        filterChainDefinitionMap.put("/login", "anon");
+        //不使用shiro自带的登出过滤器，自己实现登出
+        //filterChainDefinitionMap.put("/user/logout", "logout");
+        filterChainDefinitionMap.put("/user/login", "anon");
+        //swagger资源过滤
         filterChainDefinitionMap.put("/swagger-ui.html","anon");
         filterChainDefinitionMap.put("/static/**", "anon");
         filterChainDefinitionMap.put("/swagger/**","anon");
         filterChainDefinitionMap.put("/webjars/**", "anon");
         filterChainDefinitionMap.put("/swagger-resources/**","anon");
         filterChainDefinitionMap.put("/v2/**","anon");
-        filterChainDefinitionMap.put("/**", "authc");
-        //配置shiro默认登录界面地址，前后端分离中登录界面跳转应由前端路由控制，后台仅返回json数据
-        shiroFilterFactoryBean.setLoginUrl("/unauth");
+        filterChainDefinitionMap.put("/**","authc");
+
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
 
         return shiroFilterFactoryBean;
     }
--- src/main/java/com/xpower/iot/demo/controller/UserController.java
+++ src/main/java/com/xpower/iot/demo/controller/UserController.java
@@ -4,29 +4,32 @@ import com.xpower.iot.demo.common.base.Result;
 import com.xpower.iot.demo.common.base.ResultEnum;
 import com.xpower.iot.demo.common.base.ResultUtil;
 import com.xpower.iot.demo.service.UserService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.*;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Api(value = "/user", tags = "用户模块")
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     private UserService userService;
 
     @ApiOperation(httpMethod = "POST",value = "用户登陆", notes = "用户登陆")
-    @RequestMapping(value = "/{username}/{password}")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "username", paramType = "query", value = "hello", dataType="string", required = true),
+            @ApiImplicitParam(name = "password", paramType = "query", value = "world", dataType="string", required = true)})
+    @RequestMapping(value = "/login")
     @ResponseBody
-    public Result login(@PathVariable String username, @PathVariable String password){
+    public Result login(@ApiParam("username")String username, @ApiParam("password")String password){
         Subject subject = SecurityUtils.getSubject();
         UsernamePasswordToken token = new UsernamePasswordToken(username, password);
         try {
@@ -35,6 +38,21 @@ public class UserController {
             token.clear();
             return ResultUtil.result(ResultEnum.LOGIN_EXCEPTION, e);
         }
+        // 是否认证通过
+        boolean isAuthenticated = subject.isAuthenticated();
+        logger.info("用户：{} 认证结果：{}", username, isAuthenticated);
+
+        return ResultUtil.success();
+
+    }
+
+    @ApiOperation(httpMethod = "POST",value = "用户登出", notes = "用户登出")
+    @RequestMapping(value = "/logout")
+    @ResponseBody
+    public Result logout(){
+        Subject subject = SecurityUtils.getSubject();
+        subject.logout();
+
         return ResultUtil.success();
 
     }
